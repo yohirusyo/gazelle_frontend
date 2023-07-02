10b42858c6f0a55707b92b396213388e2501c53f
yohirusyo <yukolba817@gmail.com>
2023-07-02 13:18:28 +0500

    WIP

--- src/components/limits/History/OrderHistory.vue
+++ src/components/limits/History/OrderHistory.vue
@@ -1,16 +1,33 @@
 <template>
-  <q-table wrap-cells hide-bottom :rows-per-page-options="[0]" table-header-class="bg-white" separator="cell" class="my-sticky-header-table" dense flat
-    square :rows="history" :columns="myManagement?.isMinutes ? minutesColumns : kilometersColumns" row-key="owner"
-    v-model:expanded="expanded">
+  <q-table
+    wrap-cells
+    hide-bottom
+    :rows-per-page-options="[0]"
+    table-header-class="bg-white"
+    separator="cell"
+    class="my-sticky-header-table"
+    dense
+    flat
+    square
+    :rows="history"
+    :columns="_isMinutes ? minutesColumns : kilometersColumns"
+    row-key="owner"
+    v-model:expanded="expanded"
+  >
     <template v-slot:header="props">
-
       <q-tr :props="props">
         <q-th auto-width>
           <q-icon name="calendar_month" color="black" size="sm">
             <q-popup-proxy transition-show="scale" transition-hide="scale">
               <q-date v-model="_selectedDate" mask="YYYYMMDD" minimal range>
                 <div class="row items-center justify-end">
-                  <q-btn v-close-popup label="Применить" color="primary" flat @click="sortFetchOrders(_selectedDate)" />
+                  <q-btn
+                    v-close-popup
+                    label="Применить"
+                    color="primary"
+                    flat
+                    @click="sortFetchOrders(_selectedDate)"
+                  />
                   <q-btn v-close-popup label="Закрыть" color="primary" flat />
                 </div>
               </q-date>
@@ -31,25 +48,79 @@
 
         <q-td key="customer">{{ props.row.owner }}</q-td>
         <q-td key="orderCount">{{ props.row.orderCount }}</q-td>
-        <q-td key="limitMin" v-if="myManagement?.isMinutes">{{
+        <q-td key="limitMin" v-if="_isMinutes">{{
           toMinutes(props.row.limitMin)
         }}</q-td>
-        <q-td key="timeFine" v-if="myManagement?.isMinutes">{{
+        <q-td key="timeFine" v-if="_isMinutes">{{
           toMinutes(props.row.fine)
         }}</q-td>
-        <q-td key="limitKilo" v-if="!myManagement?.isMinutes">{{
-          (props.row.limitKilo / 1000).toFixed(1)
+        <q-td key="limitKilo" v-if="!_isMinutes">{{
+          props.row.limitKilo.toFixed(2)
         }}</q-td>
       </q-tr>
-      <q-tr v-show="props.expand" :props="props" v-for="order of props.row.orders" :key="order.id">
-        <q-td colspan="3">Номер заявки: {{ order.id }} </q-td>
-        <q-td v-if="myManagement?.isMinutes">{{
-          toMinutes(order.limitMin)
-        }}</q-td>
-        <q-td v-if="myManagement?.isMinutes">{{ toMinutes(order.fine) }}</q-td>
-        <q-td v-if="!myManagement?.isMinutes">{{
-          (order.limitKilo / 1000).toFixed(1)
-        }}</q-td>
+      <q-tr
+        v-show="props.expand"
+        :props="props"
+        v-for="order of props.row.orders"
+        :key="order.id"
+      >
+        <q-td :colspan="_isMinutes ? 3 : 2">Заявка: {{ order.id }} </q-td>
+        <q-td v-if="_isMinutes">{{ toMinutes(order.limitMin) }}</q-td>
+        <q-td v-if="_isMinutes">{{ toMinutes(order.fine) }}</q-td>
+        <q-td v-if="!_isMinutes" colspan="2">
+          <table style="width: 100%">
+            <colgroup>
+              <col span="1" style="width: 60%" />
+              <col span="1" style="width: 40%" />
+            </colgroup>
+            <tr>
+              <td>Наименование</td>
+              <td>Списано (км)</td>
+            </tr>
+            <tr v-if="order.limitKilo.customer">
+              <td>Пробег</td>
+              <td>
+                {{ order.limitKilo.kilo }}
+              </td>
+            </tr>
+            <tr v-if="order.limitKilo.customer">
+              <td>Время ожидания погрузки</td>
+              <td>
+                {{ order.limitKilo.loadingWaitingTime }}
+              </td>
+            </tr>
+            <tr v-if="order.limitKilo.customer">
+              <td>Время погрузки</td>
+              <td>
+                {{ order.limitKilo.loadingTime }}
+              </td>
+            </tr>
+            <tr v-if="order.limitKilo.customer">
+              <td>Время ожидания после погрузки</td>
+              <td>
+                {{ order.limitKilo.afterLoadingWaitingTime }}
+              </td>
+            </tr>
+            <tr v-if="order.limitKilo.cargoReciever">
+              <td>Время ожидания разгрузки</td>
+              <td>
+                {{ order.limitKilo.unloadingWaiting }}
+              </td>
+            </tr>
+            <tr v-if="order.limitKilo.cargoReciever">
+              <td>Время разгрузки</td>
+              <td>
+                {{ order.limitKilo.unloadingTime }}
+              </td>
+            </tr>
+            <tr>
+              <td>Итого</td>
+              <td>
+                {{ order.limitKilo.limit }}
+              </td>
+            </tr>
+          </table>
+        </q-td>
       </q-tr>
     </template>
   </q-table>
@@ -63,7 +134,7 @@ export default {
   components: {
     VScrolltable,
   },
-  props: ['item'],
+  props: ["item", "operatingSpeedVariable", "isMinutes"],
   data() {
     return {
       _selectedDate: null,
@@ -133,6 +204,18 @@ export default {
   computed: {
     ...mapGetters("management", ["myManagement"]),
     ...mapState("current", ["currentUser"]),
+    _isMinutes: {
+      get() {
+        return this.item ? this.isMinutes : this.myManagement?.isMinutes;
+      },
+    },
+    _operatingSpeedVariable: {
+      get() {
+        return this.item
+          ? this.operatingSpeedVariable
+          : this.myManagement?.operatingSpeedVariable;
+      },
+    },
   },
   watch: {
     _selectedDate() {
@@ -149,6 +232,10 @@ export default {
       if (!time) return "0.00";
       return (time / 60000).toFixed(2);
     },
+    toHours(millisec) {
+      if (!millisec || isNaN(Number(millisec))) return 0;
+      return Number(millisec) / (60000 * 60);
+    },
     getLimitWithFine(time) {
       if (!time) return 0;
       return time > this.fine ? (time - this.fine) * 2 + this.fine : time;
@@ -156,6 +243,58 @@ export default {
     parseNumber(number) {
       if (!number) return 0;
       return Number(number);
+    },
+    metersToKilo(meters) {
+      if (!meters || meters == 0) return 0;
+      return Number((meters / 1000).toFixed(1));
+    },
+    getKilos(stats, customerIds) {
+      const limit = {
+        cargoReciever: false,
+        customer: false,
+        limit: 0,
+        kilo: 0,
+        loadingWaitingTime: 0,
+        loadingTime: 0,
+        afterLoadingWaitingTime: 0,
+        unloadingWaiting: 0,
+        unloadingTime: 0,
+      };
+      if (customerIds.includes(stats.order.customerId)) {
+        limit.customer = true;
+        limit.kilo = this.metersToKilo(stats.order.routeLength);
+        limit.loadingWaitingTime =
+          this._operatingSpeedVariable * this.toHours(stats.loadingWaitingTime);
+        limit.loadingTime =
+          this._operatingSpeedVariable * this.toHours(stats.loadingTime);
+        limit.afterLoadingWaitingTime =
+          this._operatingSpeedVariable *
+          this.toHours(stats.afterLoadingWaitingTime);
+        limit.limit +=
+          limit.kilo +
+          limit.loadingWaitingTime +
+          limit.loadingTime +
+          limit.afterLoadingWaitingTime;
+      }
+      if (customerIds.includes(stats.order.cargoRecieverId)) {
+        limit.cargoReciever = true;
+        limit.unloadingWaiting =
+          this._operatingSpeedVariable * this.toHours(stats.unloadingWaiting);
+        limit.unloadingTime =
+          this._operatingSpeedVariable * this.toHours(stats.unloadingTime);
+        limit.limit += limit.unloadingWaiting + limit.unloadingTime;
+      }
+      limit.limit = Math.round(limit.limit * 100) / 100;
+      limit.kilo = Math.round(limit.kilo * 100) / 100;
+      limit.loadingWaitingTime =
+        Math.round(limit.loadingWaitingTime * 100) / 100;
+      limit.loadingTime = Math.round(limit.loadingTime * 100) / 100;
+      limit.afterLoadingWaitingTime =
+        Math.round(limit.afterLoadingWaitingTime * 100) / 100;
+      limit.unloadingWaiting = Math.round(limit.unloadingWaiting * 100) / 100;
+      limit.unloadingTime = Math.round(limit.unloadingTime * 100) / 100;
+      // console.log(limit);
+      return limit;
     },
     getTimeFine(item) {
       const loadingTime =
@@ -178,28 +317,29 @@ export default {
       return this.parseNumber(item.driveTime) + loadingTime + unloadingTime;
     },
     sortFetchOrders(date) {
-      this.fetchOrders({ date: date, item: this.item })
+      this.fetchOrders({ date: date, item: this.item });
     },
     async fetchOrders({ date, item }) {
-      console.log({ date, item });
-      const { data } = await api.get(item ? `/order/hierarchy/orders/${item}?managementId=${item}` : "/order/hierarchy/orders/");
+      const { data } = await api.get(
+        item ? `/order/hierarchy/orders/${item}` : "/order/hierarchy/orders/"
+      );
+      console.log(data);
       const orders = [];
-      for (const d of data) {
-
-        const orderDate = d.order.orderTime.split('T')[0].split('-').join("")
+      for (const d of data.orders) {
+        const orderDate = d.order.orderTime.split("T")[0].split("-").join("");
         if (!date || (date.from <= orderDate && orderDate <= date.to)) {
-
+          const kiloData = this.getKilos(d, data.customerIds);
           const order = {
             id: d.order.id,
             fine: this.getTimeFine(d),
             limitMin: this.getLimitMin(d),
-            limitKilo: this.parseNumber(d.order.routeLength),
+            limitKilo: kiloData,
           };
           const elem = orders.find((h) => h.owner == d.order.customer.fullname);
           if (elem) {
             elem.orderCount++;
             elem.fine += order.fine;
-            elem.limitKilo += this.parseNumber(order.limitKilo);
+            elem.limitKilo += kiloData.limit;
             elem.limitMin += order.limitMin;
             elem.orders.push(order);
           } else {
@@ -208,7 +348,7 @@ export default {
               orderCount: 1,
               fine: order.fine,
               limitMin: order.limitMin,
-              limitKilo: this.parseNumber(order.limitKilo),
+              limitKilo: kiloData.limit,
               orders: [order],
             });
           }
