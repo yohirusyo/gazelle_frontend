a49176223488817c18dbef14400e5b844d6f4506
Воробьев Алексей Павлович <VOROBEV181755@hq.corp.mmk.chel.su>
2024-04-04 07:17:37 +0500

    add loading_and_unloading_places

--- src/store/transport/mutations.js
+++ src/store/transport/mutations.js
@@ -46,3 +46,7 @@ export function setTransportRecommendationList(
 ) {
   state.transportRecommendationList = transportRecommendationList;
 }
+
+export function setPlaces(state, places) {
+  state.places = places;
+}
